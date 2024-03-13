d3e022092cb4a5693a7b57feab5c86b93796990d
guenaelinard <guenael.inard@le-campus-numerique.fr>
2024-03-13 16:44:24 +0100

    TODO : MeowSeeder, CommentSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        for ($i = 0; $i < 50; $i++) {
+            DB::table('users')->insert([
+                'name' => fake()->name,
+                'email' => fake()->safeEmail,
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'email_verified_at' => fake()->dateTime(),
+            ]);
+        }
     }
 }
